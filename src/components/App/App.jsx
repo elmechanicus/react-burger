f67c53e82ba0633ad3f8024dd57c6ccf1e70f889
Elmechanicus <elmechanicus@yandex.ru>
2022-04-07 05:31:09 +0300

    Подключить API с данными. Небольшой рефакторинг.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,18 +3,27 @@ import AppHeader from '../AppHeader/AppHeader.jsx';
 import styleApp from './app.module.css';
 import BurgerConstructor from '../BurgerIngredients/BurgerIngredients.jsx';
 import BurgerIngredients from '../BurgerConstructor/BurgerConstructor.jsx';
-import { dataArray } from '../../utils/data.jsx';
+import { getIngredients } from '../../utils/api.js';
+// import { dataArray } from '../../utils/data.jsx';
 
-function App() {
-  const cardData = JSON.stringify(dataArray);
-  const cards = JSON.parse(cardData);
-  // console.log(cards);
+function App() {                                                          
+  const [ingredients, setIngredients] = React.useState([]);
+  React.useEffect(() => {
+    getIngredients()
+      .then((res) => setIngredients(res.data))
+      .catch((err) => console.log(err))
+  }, []);
+  
+  
+  
+  // const cardData = JSON.stringify(dataArray);
+  // const cards = JSON.parse(cardData);
   return (
     <div className={styleApp.appBackground}>
       <AppHeader />
       <div className={styleApp.appContent}>
-        <BurgerConstructor ingredients={cards} />
-        <BurgerIngredients ingredients={cards} />
+        <BurgerConstructor ingredients={ingredients} />
+        <BurgerIngredients ingredients={ingredients} />
       </div>
     </div>
   );
--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -4,7 +4,8 @@ import {ConstructorElement, DragIcon, CurrencyIcon} from '@ya.praktikum/react-de
 import ingredientsStyle from './burgerConstructor.module.css'; 
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
-function BurgerConstructor(props) {
+function BurgerConstructor({ ingredients }) {
+
   return (
     <>
       <ul className={`${ingredientsStyle.grid} ml-10 pt-25 pl-4 pr-4`}>
@@ -12,30 +13,30 @@ function BurgerConstructor(props) {
           <ConstructorElement 
             type="top"
             isLocked={true}
-            text={`${props.ingredients[0].name} (верх)`}
-            price={props.ingredients[0].price}
-            thumbnail={props.ingredients[0].image}
+            text={`${ingredients.name} (верх)`}
+            price={ingredients.price}
+            thumbnail={ingredients.image}
           />
         </li>  
         <li className={`mb-4 ${ingredientsStyle.dragIcon}`}>
           <DragIcon type="primary" />
           <ConstructorElement
-            text={`${props.ingredients[1].name}`}
-            price={props.ingredients[1].price}
-            thumbnail={props.ingredients[1].image}
+            text={`${ingredients.name}`}
+            price={ingredients.price}
+            thumbnail={ingredients.image}
           />
         </li>
         <li className={`ml-8 mb-10`}>
           <ConstructorElement
             type="bottom"
             isLocked={true}
-            text={`${props.ingredients[0].name} (низ)`}
-            price={props.ingredients[0].price}
-            thumbnail={props.ingredients[0].image}
+            text={`${ingredients.name} (низ)`}
+            price={ingredients.price}
+            thumbnail={ingredients.image}
           />
         </li>
         <li className={`${ingredientsStyle.order}`}>
-          <p className='text text_type_digits-medium'>{props.ingredients[0].price * 2 + props.ingredients[1].price}<span className={`${ingredientsStyle.largeIcon}`}><CurrencyIcon type="primary" /></span></p>
+          <p className='text text_type_digits-medium'>{ingredients.price * 2 + ingredients.price}<span className={`${ingredientsStyle.largeIcon}`}><CurrencyIcon type="primary" /></span></p>
           <div className='ml-10'>
             <Button type="primary" size="large">Нажми на меня</Button>
           </div>
