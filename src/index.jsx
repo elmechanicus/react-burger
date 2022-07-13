1d3c689b0b8bfc86519b677ba929bbdca6346a8c
Elmechanicus <elmechanicus@yandex.ru>
2022-07-13 13:07:34 +0300

    Создал и подключил хранилище

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 ReactDOM.render(
   <StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </StrictMode>,
   document.getElementById('root')
 );
