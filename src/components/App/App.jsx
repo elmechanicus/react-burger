f07144af782223c5c5f8a08f0dd81c859c6010ac
Elmechanicus <elmechanicus@yandex.ru>
2022-09-20 10:27:53 +0300

    Подключил библиотеку роутинга

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,22 +4,26 @@ import BurgerIngredients  from '../BurgerIngredients/BurgerIngredients.jsx';
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor.jsx';
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
 
   return (
-    <>
-      <DndProvider backend={HTML5Backend}>
-        <div className={styleApp.appBackground}>
-          <AppHeader />
-          <div className={styleApp.appContent}>
-              <BurgerIngredients />
-              <BurgerConstructor />
-          </div>
-        </div>
-      </DndProvider>
-    </>
+    <Router>
+      <Switch>
+        <DndProvider backend={HTML5Backend}>
+          <div className={styleApp.appBackground}>
+            <AppHeader />
+              <div className={styleApp.appContent}>
+                <BurgerIngredients />
+                <BurgerConstructor />
+              </div>
+            </div>
+          </DndProvider> 
+        </Switch>
+      
+    </Router>
   );
 }
 
