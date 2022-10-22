ac5ca185753b2eae50ad8a297f422eba836129b5
Elmechanicus <elmechanicus@yandex.ru>
2022-10-22 22:54:10 +0300

    Стилизация страницы с формой регистрации

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, EmailInput, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components';
+import loginClass from './login.module.css';
 
 
 export function Login() {
@@ -15,15 +16,28 @@ export function Login() {
   }
 
   return (
-    <>
-      <h2 className='text text_type_main-medium'>Вход</h2>
-      <EmailInput onChange={onChangeEmail} value={emailValue} name={'email'} placeholder='E-mail'/>
-      <PasswordInput onChange={onChangePass} value={passValue} name={'password'} />
-      <Button type='primary' size='large' htmlType='submit'>
+    <form className={`${loginClass.content}`}>
+      <h2 className='text text_type_main-medium mb-6'>Вход</h2>
+      <div className='mb-6'>
+        <EmailInput onChange={onChangeEmail} value={emailValue} name={'email'} placeholder='E-mail'/>
+      </div>
+      <div className='mb-6'>
+        <PasswordInput onChange={onChangePass} value={passValue} name={'password'} />
+      </div>
+      <div className="mb-20">
+        <Button type='primary' size='large' htmlType='submit'>
         Войти
-      </Button>
-      <p className='text text_type_main-default text_color_inactive'>Вы - новый пользователь? Зарегистрироваться</p>
-      <p className='text text_type_main-default text_color_inactive'>Забыли пароль? Восстановить пароль</p>
-    </>
+        </Button>
+      </div>
+      <div className={`${loginClass.register}`}>
+        <p className={`${loginClass.text} text text_type_main-default text_color_inactive`}>Вы - новый пользователь?</p>
+        <div className={`${loginClass.button}`}><Button type='secondary' size='medium' htmlType='button'>Зарегистрироваться</Button></div>
+      </div>
+      <div className={`${loginClass.register}`}>
+        <p className={`${loginClass.text} text text_type_main-default text_color_inactive`}>Забыли пароль? </p>
+        <div className={`${loginClass.button}`}><Button type='secondary' size='medium' htmlType='button'>Восстановить пароль</Button></div>
+      </div>
+      
+    </form>
   )
 }
